327afa0d39ab363e3b2d02de4820604a3dcda8da
ryanberenger <ryanberenger@ryans-mbp.fios-router.home>
2021-02-09 18:09:59 -0500

    This is my House Painting code

---
+++ housePainting/src/houseCode.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class houseCode {
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		int length = 0;
+		int width = 0;
+		int height = 0;
+		int numDoors = 0;
+		int doorLength = 0;
+		int doorHeight = 0;
+		int numWindow = 0;
+		int windowLength = 0;
+		int windowHeight = 0;
+		int doorTotal = 0;
+		int windowTotal = 0;
+		int sqFtNorm = 0;
+		int sqFtPeak = 0;
+		int sqFttoPaint = 0;
+		double costPersqft = 0;
+		double costTotal = 0;
+		System.out.println("Input length of house: ");
+		length = in.nextInt();
+		System.out.println("Input width of house: ");
+		width = in.nextInt();
+		System.out.println ("Input the height of house");
+		height = in.nextInt();
+		System.out.println ("Input the number of doors ");
+		numDoors = in.nextInt();
+		System.out.println ("Input the length of the door ");
+		doorLength = in.nextInt();
+		System.out.println ("Input the width of the door ");
+		doorHeight = in.nextInt();
+		System.out.println ("Input the number of windows ");
+		numWindow = in.nextInt();
+		System.out.println("Input the length of the window ");
+		windowLength = in.nextInt();
+		System.out.println("Input the width of the window ");
+		windowHeight = in.nextInt();
+		System.out.println("Input cost to paint square foot: ");
+		costPersqft = in.nextInt();
+		doorTotal = numDoors * doorLength * doorHeight;
+		windowTotal = numWindow * windowLength * windowHeight;
+		sqFtNorm = (length * width)*2;
+		sqFtPeak = ((length * width)+((length*(height-width))/2))*2;
+		sqFttoPaint = ((sqFtNorm+sqFtPeak)-(windowTotal+doorTotal));
+		costTotal = sqFttoPaint * costPersqft;
+		System.out.println("Your total paintable surface area is " + sqFttoPaint + " square feet. ");
+		System.out.println("Cost to paint the house is " + costTotal + " dollars. ");
+		in.close();
+	}
+
+}
