47800c5be355fbb38575d8db0a5f06e8e81673aa
ryanberenger <s1304252@monmouth.edu>
2021-03-04 09:49:20 -0500

    This is my Bank Account with Modifications

---
+++ BankV2/src/BankAccount.java
@@ -0,0 +1,65 @@
+/**
+   A bank account has a balance and a mechanism for
+   applying interest or fees at the end of the month.
+*/
+public class BankAccount
+{
+	//Instance variable
+   private double balance;
+   private double interestPct;
+
+   /**
+      Constructs a bank account with zero balance.
+   */
+   public BankAccount(double startBal,double intrPct)
+   {
+      balance = startBal;
+      interestPct = intrPct;
+      System.out.println("Created a new account with "+startBal+" balance and an interest rate of "+intrPct);
+   }
+   
+   /**
+      Makes a deposit into this account.
+      @param amount the amount of the deposit
+   */
+   public void deposit(double depositAmount)
+   {
+      balance = balance + depositAmount;
+      System.out.println("Deposited: "+depositAmount);
+   }
+   
+   /**
+      Makes a withdrawal from this account, or charges a penalty if
+      sufficient funds are not available.
+      @param amount the amount of the withdrawal
+   */
+   public void withdraw(double withdrawAmount)
+   {
+   if(balance >= withdrawAmount)
+   
+      {balance = balance - withdrawAmount;
+      System.out.println("Withdrew: "+withdrawAmount);}
+   else
+	   {System.out.println("Insufficient Funds to support withdrawl");}
+   }
+	   
+   /**
+      Gets the current balance of this bank account.
+      @return the current balance
+   */
+  
+   public void calcInterest(double interestAmount)
+   {
+	   interestPct = balance * interestPct;
+	   balance = balance + interestPct;
+	   if ("response" != null)
+	   {System.out.println("Interest: "+interestPct);}
+   }
+   
+   
+   
+   public double getBalance()
+   {
+      return balance;
+   }
+}
