53baef622770e1e6021953b0bd73477d5b43efdb
ryanberenger <s1304252@monmouth.edu>
2021-04-01 13:40:41 -0400

    This is the updated version of my Bank Threshold Code

--- Bank/src/BankThreshold.java
+++ Bank/src/BankThreshold.java
@@ -9,16 +9,18 @@ double startBal = in.nextDouble();
 System.out.print("Enter the interest rate for the account: ");
 double interestAmount = in.nextDouble();
 BankAccount myBankAccount = new BankAccount(startBal,interestAmount);
-System.out.print("Enter the threshold for your account balance to reach: ");
-int months = 0;
+System.out.print("Enter the threshold for your account balance to reach or 'end': ");
 while (in.hasNextDouble())
 {
 	double thresholdBal = in.nextDouble();
+	int months = 0;
 	while (myBankAccount.getBalance() < thresholdBal)
 	{
 		myBankAccount.calcInterest();
 		months++;
 	}	
+	myBankAccount.resetBalance(startBal);
+	
 	System.out.println("It will take " +months+ " months to reach the threshold");
 }
 			}
